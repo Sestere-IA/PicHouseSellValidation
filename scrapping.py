d4ef8653c379078f85176e7446053ddec08434af
NABYTECH <alexandre.nabyt75@gmail.com>
2022-04-04 13:37:18 +0200

    add some stucture to project

---
+++ scrapping.py
@@ -0,0 +1,17 @@
+"""
+All tool to scrap
+"""
+
+
+class ScrappingPicture:
+    """
+    Scrapping picture from web site
+    """
+
+    def __init__(self):
+        pass
+
+    # TODO Scrapping web site 1 time per week to be able to get tendance of best selling pictures
+    # TODO get all pic for compliance pic
+    # Validate compliance and/or tendance of picture posted
+    # TODO Host .py and run it 1 time/da
