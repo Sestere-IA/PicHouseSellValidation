89c04cfda7679e9f4e105f44793e5356321de743
Sestere <alexandre.nabyt75@gmail.com>
2022-05-27 09:47:37 +0200

    Stucture  + Scrapping "SeLoger"

--- scrapping.py
+++ scrapping.py
@@ -1,6 +1,8 @@
 """
 All tool to scrap
 """
+from scrapy.crawler import CrawlerProcess
+from generate_csv import PicWebSite
 
 
 class ScrappingPicture:
@@ -9,7 +11,13 @@ class ScrappingPicture:
     """
 
     def __init__(self):
-        pass
+        # run spider
+        process = CrawlerProcess(settings={
+            'FEED_URI': 'name_company_link_company_size.csv',
+            'FEED_FORMAT': 'csv'
+        })
+        process.crawl(PicWebSite)
+        process.start()
 
     # TODO Scrapping web site 1 time per week to be able to get tendance of best selling pictures
     # TODO get all pic for compliance pic
